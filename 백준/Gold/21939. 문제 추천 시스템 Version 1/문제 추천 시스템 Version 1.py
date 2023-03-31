b3725eef824e260b96b3d9899993aea360602ac1
thdus17 <82258923+thdus17@users.noreply.github.com>
2023-03-31 19:03:29 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 312 ms, Memory: 67416 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
@@ -7,26 +7,26 @@ from collections import defaultdict
 N = int(input())
 min_heap = []
 max_heap = []
-in_list = defaultdict(bool)
+in_list = defaultdict(bool) #value 타입 bool 로 제한
 for _ in range(N):
     P, L = map(int, input().split())
-    heappush(min_heap,[L,P])
-    heappush(max_heap,[-L,-P])
-    in_list[P] = True
-
+    heappush(min_heap,[L,P]) #쉬운 문제 추천을 위한 heap
+    heappush(max_heap,[-L,-P]) #어려운 문제 추천을 위한 heap
+    in_list[P] = True #해당 문제 번호를 키로 True 값 넣기
+#in_list 딕셔너리
 M = int(input())
 for _  in range(M):
     command = input().split()
     if command[0]=='recommend':
         if command[1]=='1':
-            while not in_list[-max_heap[0][1]]:
+            while not in_list[-max_heap[0][1]]: #False 처리된 애들 차례로 뱉어내기
                 heappop(max_heap)
             print(-max_heap[0][1])
         else:
-            while not in_list[min_heap[0][1]]:
+            while not in_list[min_heap[0][1]]: #False 처리된 애들 차례로 뱉어내기
                 heappop(min_heap)
             print(min_heap[0][1])
-    elif command[0]=='solved':
+    elif command[0]=='solved': #푼 문제 False 로 값 변경
         in_list[int(command[1])] = False
     else:
         P = int(command[1])
