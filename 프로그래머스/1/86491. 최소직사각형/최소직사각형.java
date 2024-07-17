4492a8fcff48ef992d64b72c6d3903aae04d95d5
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-07-17 17:50:27 +0900

    [level 1] Title: 최소직사각형, Time: 2.06 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
         /*
 * 1. 내부 배열 오름차순 정렬
 * 2. 가로 길이만 뽑아내기
@@ -10,30 +9,12 @@ class Solution {
 * 3. 내림차순 정렬해서 각각 최대값 뽑기
 *
 * */
-        List<List<Integer>> listSizes = new ArrayList<>();
-        for (int[] size: sizes) {
-            List<Integer> listSize = new ArrayList<>();
-            listSize.add(Integer.valueOf(size[0]));
-            listSize.add(Integer.valueOf(size[1]));
-            listSizes.add(listSize);
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
-
-        Integer width = 0;
-        Integer height = 0;
-        if (listSizes.get(0).get(0) >listSizes.get(0).get(1)) {
-            width = listSizes.get(0).get(0);
-            height = listSizes.get(0).get(1);
-        } else {
-            width = listSizes.get(0).get(1);
-            height = listSizes.get(0).get(0);
-        }
-        for (List<Integer> size: listSizes) {
-            Collections.sort(size);
-            width = width < size.get(1) ? size.get(1):width;
-            height = height < size.get(0) ? size.get(0):height;
-        }
-
-        answer = width*height;
+        int answer = length * height;
         return answer;
     }
 }
