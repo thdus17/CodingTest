65cf18dcc04963b8289cb4148102e83cb2545bff
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-10 15:45:34 +0900

    [level 1] Title: 폰켓몬, Time: 7.24 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,34 +2,17 @@ import java.util.HashMap;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        HashMap<Integer, Integer> answerKey = new HashMap<>();
         HashMap<Integer, Integer> pocketmons = new HashMap<>();
-        for (int num: nums) {
+        for (int num : nums) {
             if (pocketmons.containsKey(num)) {
-                pocketmons.put(num, pocketmons.get(num)+1);
+                pocketmons.put(num, pocketmons.get(num) + 1);
             } else {
                 pocketmons.put(num, 1);
             }
         }
 
-        final int[] takingCnt = {nums.length / 2};
-        if (takingCnt[0] <= pocketmons.size()) {
-            return takingCnt[0];
-        }
-        while (takingCnt[0] > 0) {
-            pocketmons.forEach( (key, value) -> {
-                if (value != 0) {
-                    takingCnt[0] -= 1;
-                    pocketmons.put(key, value-1);
-                    if (!answerKey.containsKey(key)) {
-                        answerKey.put(key, 1);
-                    } else {
-                        answerKey.put(key, answerKey.get(key)+1);
-                    }
-                }
-            });
-        }
-        answer = answerKey.size();
+        answer = Math.min(nums.length / 2, pocketmons.size());
+
         return answer;
     }
 }
