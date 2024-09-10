7563be5b93ef60256c53fcfcff3f964b9c3fc41d
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-10 17:18:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.30 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
-
+import java.util.Iterator;
+import java.util.Map;
 
 
 class Solution {
@@ -11,12 +12,16 @@ class Solution {
         for (String player : completion)
             map.put(player, map.get(player) - 1);
 
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                answer = key;
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if (entry.getValue() != 0){
+                answer = entry.getKey();
                 break;
             }
         }
+        
         return answer;
     }
 }
