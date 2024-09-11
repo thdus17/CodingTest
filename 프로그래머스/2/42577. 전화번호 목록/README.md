107725eceafab52c7cfb99b5149486867fa3a765
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-11 11:26:06 +0900

    [level 2] Title: 전화번호 목록, Time: 308.60 ms, Memory: 219 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 98.9 MB, 시간: 349.65 ms
+메모리: 219 MB, 시간: 308.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 11일 10:16:26
+2024년 09월 11일 11:27:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,4 +1,9 @@
 import java.util.*;
+/**
+1. 해시맵에 key값으로 전화번호 저장
+2. 한 전화번호를 기준으로 (0~n) 문자열을 0-1~0-n-1으로 slicing하여 전화번호부 내에 그 값이 존재하는지 확인
+3. 존재하면 바로 false 반환
+*/
 class Solution {
     public boolean solution(String[] phone_book) {
         
@@ -10,7 +15,7 @@ class Solution {
         
         
         for (int i = 0; i < phone_book.length; i++)
-            for (int j = 0; j < phone_book[i].length(); j++)
+            for (int j = 1; j < phone_book[i].length(); j++)
                 if (map.containsKey(phone_book[i].substring(0, j)))
                     return false;
 
