769f68cfbe2a2148ae087a45b28f4ad41f7f2d05
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-11 15:51:43 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,13 +1,6 @@
-/**
-     * 1. 종류(1)를 키값으로 의상 이름 배열에 저장하기 hashMap
-     * 2. 최소 1개의 의상은 장착. 1개~n개(의상 종류) 장착할 경우로 계산하기
-     * 3. 1개 장착할 경우, 모든 제품 개수
-     * 4. 종류 n개 장착할 경우... 흠 각 종류의 제품 개수끼리 곱하면 됨
-     * @return
-     */
-
 import java.util.ArrayList;
 import java.util.HashMap;
+
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
