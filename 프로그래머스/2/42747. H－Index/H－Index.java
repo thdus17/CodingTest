6cc375246145f3b1a4ee5b330e4fbab173bc24d4
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-12 16:43:44 +0900

    [level 2] Title: H-Index, Time: 3.00 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,16 +1,24 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
-        //논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index
-        Arrays.sort(citations); // 인용 수를 오름차순으로 정렬
-        int n = citations.length;
+        Arrays.sort(citations); 
+        int n = citations.length; 
 
-        for (int h = 0; h <= n; h++) {
-            // h편의 논문이 h번 이상 인용되었는지 확인
-            if (h == n || citations[n - h-1] >= h) {
-                return h; // H-Index 반환
+        for (int h = n; h >= 0; h--) { // H-Index를 n부터 0까지 체크
+            int count = 0; 
+            
+            // h번 이상 인용된 논문 수 세기
+            for (int citation : citations) {
+                if (citation >= h) {
+                    count++;
+                }
+            }
+
+            // H-Index 조건 체크
+            if (count >= h) {
+                return h; 
             }
         }
-        return 0; // 기본값 (모든 논문의 인용수가 0인 경우)
+        return 0; // 모든 논문의 인용수가 0인 경우
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 71 MB, 시간: 2.51 ms
+메모리: 68.2 MB, 시간: 3.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 12일 16:42:44
+2024년 09월 12일 16:45:18
 
 ### 문제 설명
 
