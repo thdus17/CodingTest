cc3ac3c51779166b9feb2db37698860f908bd7e4
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-12 17:28:48 +0900

    [level 2] Title: H-Index, Time: 7.02 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,22 +1,16 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations); 
-        int n = citations.length; 
+        Arrays.sort(citations);
+        int n = citations.length;
 
         for (int h = n; h >= 0; h--) { // H-Index를 n부터 0까지 체크
-            int count = 0; 
-            
-            // h번 이상 인용된 논문 수 세기
-            for (int citation : citations) {
-                if (citation >= h) {
-                    count++;
-                }
-            }
+            int finalH = h;
+            int count = (int) Arrays.stream(citations).filter(x -> x >= finalH).count();
 
             // H-Index 조건 체크
             if (count >= h) {
-                return h; 
+                return h;
             }
         }
         return 0; // 모든 논문의 인용수가 0인 경우
