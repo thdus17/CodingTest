523ef078be6843ce5e74068e6f724836cd6a4227
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-19 16:24:05 +0900

    [level 2] Title: 기능개발, Time: 0.27 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,15 +1,49 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 class Solution {
   public int[] solution(int[] progresses, int[] speeds) {
-      int[] dayOfend = new int[100];
-      int day = -1;
-      for(int i=0; i<progresses.length; i++) {
-          while(progresses[i] + (day*speeds[i]) < 100) {
-              day++;
-          }
-          dayOfend[day]++;
-      }
-      return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+      int[] answer = {};
+        int[] dayArr = new int[progresses.length];
+        ArrayList<Integer> answerList = new ArrayList<>();
+
+        for(int i=0; i<progresses.length; i++) {
+            int pgs = 100 - progresses[i];
+            int day = pgs/speeds[i] + (pgs%speeds[i] > 0 ? 1: 0);
+            dayArr[i] = day;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(dayArr[0]);
+
+        for(int i=1; i<dayArr.length; i++) {
+            int target = dayArr[i];
+            if(target <= queue.peek()) {
+                queue.add(target);
+            }else {
+                int cnt = 0;
+                while(!queue.isEmpty()) {
+                    queue.poll();
+                    cnt++;
+                }
+                answerList.add(cnt);
+
+                queue.add(target);
+            }
+        }
+
+        if(!queue.isEmpty()) {
+            int cnt = 0;
+            while(!queue.isEmpty()) {
+                queue.poll();
+                cnt++;
+            }
+            answerList.add(cnt);
+        }
+
+        answer = new int[answerList.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
   }
 }
