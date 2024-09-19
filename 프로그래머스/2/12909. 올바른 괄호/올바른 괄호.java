ba3558d2c9fc7634a16f2870ae44b945a845354a
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-19 17:08:53 +0900

    [level 2] Title: 올바른 괄호, Time: 8.66 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,23 @@
 import java.util.Stack;
 class Solution {
     boolean solution(String s) {
-        /**
-         * 1. stack에 하나씩 잘라서 넣기
-         * 2. 만약 넣는 게 ) 닫힌 괄호면 pop 두 번하기
-         * 3. 만약 pop 두 번이 불가하면 answer = false; break;
-         * 4.
-         */
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                stack.push(ch); 
-            } else {
-                if (stack.isEmpty()) {
-                    return false; 
-                }
-                stack.pop(); 
+        boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
             }
         }
+        if(count == 0){
+            answer = true;
+        }
 
-        return stack.isEmpty(); // 스택이 비어있으면 모든 괄호가 짝이 맞음
+        return answer;
     }
 }
