ce11746a2e90e44266dbe7739d3abb3a43066285
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-19 11:39:50 +0900

    [level 2] Title: 기능개발, Time: 3.71 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,29 +1,15 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> answerList = new ArrayList<>();
-
-        for (int i = 0; i < speeds.length; i++) {
-            double remain = (100 - progresses[i]) / (double) speeds[i];
-            int date = (int) Math.ceil(remain);
-
-            if (!q.isEmpty() && q.peek() < date) {
-                answerList.add(q.size());
-                q.clear();
-            }
-
-            q.offer(date);
-        }
-
-        answerList.add(q.size());
-
-        int[] answer = new int[answerList.size()];
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = answerList.get(i);
-        }
-
-        return answer;
-    }
+  public int[] solution(int[] progresses, int[] speeds) {
+      int[] dayOfend = new int[100];
+      int day = -1;
+      for(int i=0; i<progresses.length; i++) {
+          while(progresses[i] + (day*speeds[i]) < 100) {
+              day++;
+          }
+          dayOfend[day]++;
+      }
+      return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+  }
 }
