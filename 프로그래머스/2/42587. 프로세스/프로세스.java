e9ce0e5834397ee5e7219bdb2c44861c30a6f6b2
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-20 17:01:25 +0900

    [level 2] Title: 프로세스, Time: 5.21 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,6 +3,15 @@ import java.util.Arrays;
 import java.util.Comparator;
 class Solution {
     public int solution(int[] priorities, int location) {
+        /**
+         * 1. copy 후 내림차순 정렬해서 max값 차례로 꺼내 쓰는 것으로
+         * 2. actCnt=0 (실행몇번째인지) cntIdx = 0 (idx 체크용) >> n으로 나눈 나머지값이 인덱스값, cnt 0일 때만 예외처리
+         *    actFlag[] (해당 idx 실행시킨 작업인지 체크용) 
+         * 3. 
+         * 4. prioritiesArr[actCnt] == priority[idx]일 때 actCnt++, actFlag[idx] = true처리
+         * 4-1. 같을 때 location == idx 면 break;
+         */
+
         Integer[] priorityArr = Arrays.stream(priorities).boxed().toArray(Integer[]::new);
         Arrays.sort(priorityArr, Comparator.reverseOrder());
 
