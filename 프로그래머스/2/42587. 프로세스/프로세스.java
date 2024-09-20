f6ed6f670be9b1387971f4a31ae772bea9ed2e4f
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-09-20 17:03:44 +0900

    [level 2] Title: 프로세스, Time: 1.21 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,35 +1,36 @@
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        /**
-         * 1. copy 후 내림차순 정렬해서 max값 차례로 꺼내 쓰는 것으로
-         * 2. actCnt=0 (실행몇번째인지) cntIdx = 0 (idx 체크용) >> n으로 나눈 나머지값이 인덱스값, cnt 0일 때만 예외처리
-         *    actFlag[] (해당 idx 실행시킨 작업인지 체크용) 
-         * 3. 
-         * 4. prioritiesArr[actCnt] == priority[idx]일 때 actCnt++, actFlag[idx] = true처리
-         * 4-1. 같을 때 location == idx 면 break;
-         */
+        int answer = 0;
+        int l = location;
 
-        Integer[] priorityArr = Arrays.stream(priorities).boxed().toArray(Integer[]::new);
-        Arrays.sort(priorityArr, Comparator.reverseOrder());
-
-        boolean[] actFlag = new boolean[priorities.length];
-        int cntIdx = 0;
-        int actCnt = 0;
-
-        while (true) {
-            int idx = cntIdx!=0 ? cntIdx%priorities.length:0;
-            if (priorityArr[actCnt] == priorities[idx] && !actFlag[idx]) {
-                actCnt ++;
-                actFlag[idx] = true;
-                if (idx == location) {
-                    break;
-                }
-            }
-            cntIdx ++;
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int i : priorities){
+            que.add(i);
         }
-        return actCnt;
+
+        Arrays.sort(priorities);
+        int size = priorities.length-1;
+
+
+
+        while(!que.isEmpty()){
+            Integer i = que.poll();
+            if(i == priorities[size - answer]){
+                answer++;
+                l--;
+                if(l <0)
+                    break;
+            }else{
+                que.add(i);
+                l--;
+                if(l<0)
+                    l=que.size()-1;
+            }
+        }
+
+        return answer;
+
     }
 }
