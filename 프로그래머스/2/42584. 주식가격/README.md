8f45c8cc5c55474f48b2aa5fc22acdd3187c10e8
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-10-08 11:22:03 +0900

    [level 2] Title: 주식가격, Time: 19.97 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 32.18 ms
+메모리: 71.3 MB, 시간: 19.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 11:19:29
+2024년 10월 08일 11:24:06
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,31 +1,14 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
 class Solution {
     public int[] solution(int[] prices) {
-        /**
-         * 1. 맨 마지막은 무조건 0
-         * 2. que 사용 poll 로 맨 앞 당기고 그 값을 que 뒤의 값들과 >= 개수 체크
-         *
-         */
         int[] answer = new int[prices.length];
-        Queue<Integer> stock = new ArrayDeque<>();
-        for (int n : prices) {
-            stock.add(n);
-        }
-
-        int nowIdx = 1;
-        int target = stock.poll();
-        while (!stock.isEmpty()) {
-            int sec = 0;
-            for (int i = nowIdx; i < prices.length; i++) {
-                sec++;
-                if (prices[i]-target < 0) {
+        
+        for (int i = 0 ; i < prices.length-1;i++) {
+            for (int j = i+1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
                     break;
                 }
             }
-            answer[nowIdx-1] = sec;
-            nowIdx++;
-            target = stock.poll();
         }
         answer[prices.length-1] = 0;
         return answer;
