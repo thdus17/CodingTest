17e385a051a3a40c10099fac9faff27988242f55
thdus17 <82258923+thdus17@users.noreply.github.com>
2024-10-08 17:42:11 +0900

    [level 4] Title: 징검다리, Time: 35.52 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -4,12 +4,7 @@ import java.util.Arrays;
 public class Solution {
     public int solution(int distance, int[] rocks, int n) {
         /**
-         * 바위 사이 거리들 중 최소값이지만 그 중에서 제일 최대값
-         * 조건
-         * distance는 1 이상 1,000,000,000 이하
-         * 바위는 1개 이상 50,000개 이하
-         * n 은 1 이상 바위의 개수 이하
-         *
+         * 
          * 돌 사이 간격 배열에 미리 저장!
          * 초기 left = 1 right = 길이
          * mid를 설명할 때 임의로 돌을 제거한 돌들 간격의 최소거리라고 가정
@@ -17,8 +12,7 @@ public class Solution {
          * 제거한 개수가 n보다 크면 right 값을 mid-1로 변경
          * 제거한 개수가 n과 같은 때, 제거한 개수가 더 작을 때를 같이 묶어서 고려 (중요)
          * 제거한 개수 < n의 경우, mid값을 증가시켜야 함 left = mid + 1
-         * 제거한 개수 == n의 경우에도 마찬가지로 처리, 대신 answer에 저장해두기
-         * 즉, 현재 mid값에서 제거한 갯수 == n이 더라도
+         * 제거한 개수 == n의 경우에도 마찬가지로 처리 최대값을 찾아야 하므로 Math.max(answer, mid)
          * 이 mid값이 n개만큼 돌을 제거하였을 때 가질 수 있는 돌 사이 최소 간격의 최대값임을 보장할 수는 없다는 것이다.
          * 따라서 이 경우에도 mid값을 더 크게하여 이진탐색을 진행하여야 한다.
          *
@@ -27,7 +21,7 @@ public class Solution {
         int answer = 0;
 
         Arrays.sort(rocks);
-        int left = 1;
+        int left = 0;
         int right = distance;
 
         int[] difByrock = new int[rocks.length+1];
